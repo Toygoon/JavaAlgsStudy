78c3763e0ca89fd013c8916f370f1c0acb57b99e
Toygoon <toygoon@gmail.com>
2022-08-27 12:13:24 +0900

    Q003 : Scanner 대신 BufferedReader 사용

--- Chap03/Q003.java
+++ Chap03/Q003.java
@@ -1,18 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Q003 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] tmp = sc.nextLine().split(" ");
+        String[] tmp = br.readLine().split(" ");
         // 총 데이터의 개수 (쓰이지 않음)
         int N = Integer.parseInt(tmp[0]);
         // 합을 구해야 하는 개수
         int M = Integer.parseInt(tmp[1]);
 
         // 데이터 배열 입력
-        int[] data = Arrays.stream(("0 " + sc.nextLine()).split(" ")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+        int[] data = Arrays.stream(("0 " + br.readLine()).split(" ")).map(String::trim).mapToInt(Integer::parseInt).toArray();
 
         // 구간 합 배열 계산
         for (int i = 1; i < data.length; i++)
@@ -20,7 +22,7 @@ public class Q003 {
 
         // 구간을 입력 받은 후, 출력
         for (int i = 0; i < M; i++) {
-            tmp = sc.nextLine().split(" ");
+            tmp = br.readLine().split(" ");
             System.out.println(data[Integer.parseInt(tmp[1])] - data[Integer.parseInt(tmp[0]) - 1]);
         }
     }
